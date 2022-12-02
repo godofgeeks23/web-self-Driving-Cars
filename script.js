b0d2310597be08340e28d5ca435f12a7b3628b33
Aviral Srivastava <aviralji4@gmail.com>
2022-12-02 22:29:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-const canvas = document.getElementById('mycanvas');
-canvas.width = 200;
-canvas.height = 200;
+function val(x) {
+    return x + 10;
+}
+function sum(a, b = val(a)) {
+    return a + b;
+}
+console.log(sum(10));
