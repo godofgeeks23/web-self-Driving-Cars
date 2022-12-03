6e97fb97653b67037edd7e94adb6d51636a5d761
Aviral Srivastava <aviralji4@gmail.com>
2022-12-03 23:05:46 +0530

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup() { 
+      
+    // Create Canvas of given size 
+    createCanvas(400, 300); 
+  
+} 
+  
+function draw() { 
+      
+    background(220);
+      
+    // Use color() function
+    let c = color('green');
+  
+    // Use fill() function to fill color
+    fill(c);
+    
+    // Draw a circle 
+    circle(200, 100, 150); 
+    
+}
